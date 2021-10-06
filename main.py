80ef822f794391370c9a977d6ec1d827cd0608da
Ambz2 <ameenahmed1998@gmail.com>
2021-10-06 18:15:35 +0100

    edited operations file and added new stuff

--- main.py
+++ main.py
@@ -10,25 +10,26 @@ options = {"quit": 0, "open account": 1, "view account": 2, "open committee": 3,
 print(welcome)
 print("Do you already have an account?")
 has_account = input("Please enter Yes or No: ")
-
+logged_in = [False, ""]
 if has_account.lower() == 'yes': ## if they have an account, they will need to log in
     print("Please log into our systems.")
     logged_in = login()
 else:
     print("Please sign up for an account to continue.")
-    functions.create_user()
+    create_user()
 
 
+while True:
+    if logged_in[0]:
+        print("Please select one of the following options:")
+        for option in options.keys():
+            print("\t" + option.title())
 
-while logged_in[1]:
-    print("Please select one of the following options:")
-    for option in options.keys():
-        print("\t" + option.title())
 
-
-    selected = get_option()
-    director(selected)
-
+        selected = get_option()
+        director(selected)
+    else:
+        logged_in = login()
 ## need to rewrite the above code into a function which converts the entry into a
 ## number and then send us the right way
 
--- operations.py
+++ operations.py
@@ -3,18 +3,23 @@ import json
 from datetime import datetime
 import random
 
+options = {"quit": 0, "view account": 1, "open committee": 2,
+           "calculate contributions": 3, "view credit scores": 4
+           }
+
 def login():
     notlogin = True
-    while notlogin = True:
+    while notlogin:
         login_name = input("Please enter your username: ")
         password = input("Please enter your password: ")
         data = open_database()
         if login_name in data.keys():
             if data[login_name]["password"] == password:
-                print("Log in successful. Welcome back " + login_name + ".")
                 notlogin = False
-                print(notlogin)
+                print("Log in successful. Welcome back " + login_name + ".")
                 return [True, login_name]
+
+
             else:
                 print("Username or password incorrect. Please try again.")
         else:
@@ -39,18 +44,30 @@ def create_user():
 
     print("Account created.")
 
+def create_new_user():
+    """Add's a user to the database."""
+    data = open_database()
+    username = username_checker(data)
+    password = input("Please input a password: ")
+    first_name = input("Please input your first name: ")
+    last_name = input("Please input your last name: ")
+    dob = input("Please input your date of birth: ")
+    write_to_database(username, password, first_name, last_name, dob, data)
+
+    print("Account created.")
+
 
 def write_to_database(username, password, first_name, last_name, dob, data):
     """Writes the username to the database"""
-    data = open_database()
-    database[username] = {}
-    database[username]["password"] = password
-    database[username]["first_name"] = first_name
-    database[username]["last_name"] = last_name
-    database[username]["DOB"] = dob
-    database[username]["Account_Creation_Date"] = get_creation_date()
+
+    data[username] = {}
+    data[username]["password"] = password
+    data[username]["first_name"] = first_name
+    data[username]["last_name"] = last_name
+    data[username]["DOB"] = dob
+    data[username]["Account_Creation_Date"] = get_creation_date()
     with open("database.json", "r+") as file:
-        data = json.load(file)
+        database = json.load(file)
         data.update(database)
         file.seek(0)
         json.dump(data, file, indent=4, sort_keys = True, default=str)
@@ -74,6 +91,7 @@ def get_option():
     while selection.lower() not in options:
         print("Sorry, that option doesn't look correct."
               "Please try again")
+    return options[selection.lower()]
 
 
 def director(selected):
@@ -81,13 +99,14 @@ def director(selected):
     if selected == 0:
         sys.exit()
     elif selected == 1:
-        create_user()
-    elif selected == 2:
         view_account() #need to complete
-    elif selected == 3:
+    elif selected == 2:
         open_committee() #need to complete
-    elif selected == 4:
+    elif selected == 3:
         calculate_contributions() #need to complete
-    elif selected == 5:
+    elif selected == 4:
         view_scores() #need to complete
 
+def view_account(data, login_name):
+    """Allows the user to view their own account once they have logged in."""
+    print(data[login_name])

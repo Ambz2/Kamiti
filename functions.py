3772aa08918443e84b1e31e690420f00cf514998
Ambz2 <91469097+Ambz2@users.noreply.github.com>
2021-10-06 14:42:13 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,19 +1,23 @@
 import sys
 import json
+from datetime import datetime
+import random
+
 
 database = {}
 options = {"quit": 0, "open account": 1, "view account": 2, "open committee": 3,
            "calculate contributions": 4, "view credit scores": 5,
            }
 
-def login():
+def login():  ##NEEDS TO BE REFORMULATED
     """Logs the user in, and returns true once password matches"""
     login_name = input('Enter username: ')
     ## Open's JSON dictionary and loads it into python.
+    data = open_database()
     if login_name in data.keys(): ## check if username is in database
         while True:
             password = input('Enter password: ')
-            if password in data[login_name]: #checks if password matches
+            if password in data[login_name]['password']: #checks if password matches
                 print("Welcome " + str(login_name) + ".")
                 return True
             else:
@@ -24,34 +28,47 @@ def login():
 
 def create_user():
     """Add's a user to the database."""
-    while True:
-        username = input("Please input a username: ")
-        if username in database.keys():
-            print("Sorry that username is already taken. "
-                  "\nPlease choose another one")
-            continue
-        else:
-            break
+    data = open_database()
+    username = username_chooser(data)
     password = input("Please input a password: ")
-    database[username] = password
-    write_to_database()
+    first_name = input("Please input your first name: ")
+    last_name = input("Please input your last name: ")
+    dob = input("Please input your date of birth: ")
+    write_to_database(username, password, first_name, last_name, dob, data)
 
     print("Account created.")
 
-
+def username_chooser(data):
+    while True:
+        username = input("Please input a username: ")
+        if username in data.keys():
+            print("Sorry, that is already in use. \nPlease choose another.")
+            continue
+        else:
+            break
+    return username
 
 def open_database():
     """Opens up the database"""
     with open("database.json","r") as file:
         data = json.load(file)
+    return data
 
-def write_to_database():
-    """Writes the username to the database"""0
+def write_to_database(username, password, first_name, last_name, dob, data):
+    """Writes the username to the database"""
+    data = open_database()
+    database[username] = {}
+    database[username]["password"] = password
+    database[username]["first_name"] = first_name
+    database[username]["last_name"] = last_name
+    database[username]["DOB"] = dob
+    database[username]["Account_Creation_Date"] = get_creation_date()
+    database[username]["Customer_ID_Number"] = id_generator(data)
     with open("database.json", "r+") as file:
         data = json.load(file)
         data.update(database)
         file.seek(0)
-        json.dump(data, file)
+        json.dump(data, file, indent=4, sort_keys = True, default=str)
 
 def get_option():
 
@@ -78,7 +95,35 @@ def director(selected):
     elif selected == 5:
         view_scores() #need to complete
 
+def get_creation_date():
+    """Returns the date when the account was created."""
+    current_datetime = datetime.now().date()
+    return current_datetime
 
+def id_generator(data):
+    """Generates an id number for each user using a random int
+    which is then compared to other users"""
+    print(data)
+    while True:
+        id_number = random.randint(1, 4)
+        for dictionary in data:
+            if str(id_number) not in dictionary:
+                return id_number
+                break
+            else:
+                continue
+def view_account(data, login_name):
+    """Allows the user to view their own account once they have logged in."""
+    print(data[login_name])
+
+
+#def open_committee():
+
+
+#def calculate_contributions():
+
+
+#def view_scores():
 
 
 
